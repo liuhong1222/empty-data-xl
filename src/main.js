106de98d928fad6e215df683de680e8698dbc704
wansh <wansh@253.com>
2021-12-29 14:07:48 +0800

    feat: IE11以下建议更换浏览器

--- src/main.js
+++ src/main.js
@@ -76,6 +76,38 @@ function loadJS (src) {
   s.parentNode.insertBefore(hm, s)
 }
 
+function dealIE (data) {
+  var theUA = window.navigator.userAgent.toLowerCase()
+  if (
+    (theUA.match(/msie\s\d+/) && theUA.match(/msie\s\d+/)[0]) ||
+    (theUA.match(/trident\s?\d+/) && theUA.match(/trident\s?\d+/)[0])
+  ) {
+    var ieVersion =
+      theUA.match(/msie\s\d+/)[0].match(/\d+/)[0] ||
+      theUA.match(/trident\s?\d+/)[0]
+    if (ieVersion < 11) {
+      console.log('data---' + data)
+      var str = '你的浏览器版本太low了,已经和时代脱轨了 :'
+      var str2 =
+        "推荐使用:<a href='https://www.baidu.com/s?ie=UTF-8&wd=%E8%B0%B7%E6%AD%8C%E6%B5%8F%E8%A7%88%E5%99%A8' target='_blank' style='color:blue;'>谷歌</a>," +
+        "<a href='https://www.baidu.com/s?ie=UTF-8&wd=%E7%81%AB%E7%8B%90%E6%B5%8F%E8%A7%88%E5%99%A8' target='_blank' style='color:blue;'>火狐</a>," +
+        '其他双核极速模式'
+      var str3 = '联系方式：' + data.data.domainContactWay || ''
+      document.writeln(
+        "<pre style='text-align:center;color:#fff;background-color:rgb(56, 120, 172); height:100%;border:0;position:fixed;top:0;left:0;width:100%;z-index:1234'>" +
+          "<h2 style='padding-top:200px;margin:0'><strong>" +
+          str +
+          '<br/></strong></h2><h2>' +
+          str2 +
+          "</h2><h2 style='margin:0'><strong>如果你使用的是双核浏览器,请切换到极速(高速)模式 / 兼容模式访问，如果仍然不能访问，请考虑以上两条建议<br/></strong></h2><h2><strong>" +
+          str3 +
+          '</h2></strong></pre>'
+      )
+      document.execCommand('Stop')
+    }
+  }
+}
+
 // 查看代理商网站设置
 server
   .siteInfo({})
@@ -83,6 +115,9 @@ server
     if (data.code === 200) {
       var siteInfo = data.data || {}
       Vue.prototype.siteInfo = siteInfo
+      // 处理ie11以下
+      dealIE(data)
+
       document.title = siteInfo.siteName || ''
       // document.querySelector("link[rel*='icon']").href =
       //   Vue.prototype.downloadDomain + siteInfo?.agentIcon || ''
